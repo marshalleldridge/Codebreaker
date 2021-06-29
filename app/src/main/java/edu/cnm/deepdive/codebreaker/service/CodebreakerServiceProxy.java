7f6a5f670661c95410819de45ac1a7da1d305296
Marshall Eldridge <l2marshall@gmail.com>
2021-06-29 14:38:23 -0600

    testing roundtrip.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerServiceProxy.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerServiceProxy.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.codebreaker.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.codebreaker.BuildConfig;
 import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.model.Guess;
 import io.reactivex.Single;
@@ -75,7 +76,7 @@ public interface CodebreakerServiceProxy {
           .create();
 
       HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-      interceptor.setLevel(Level.NONE);
+      interceptor.setLevel(BuildConfig.DEBUG ? Level.BODY : Level.NONE);
       OkHttpClient client = new OkHttpClient.Builder()
           .addInterceptor(interceptor)
           .build();
