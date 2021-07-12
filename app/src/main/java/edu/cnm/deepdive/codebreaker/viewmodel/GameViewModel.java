9fc3b26f2ae8d11cf629d7d3956199919e09ee0f
Marshall Eldridge <l2marshall@gmail.com>
2021-07-12 13:54:44 -0600

    Spinner working with preferences.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
@@ -18,19 +18,18 @@ import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.service.GameRepository;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
-import org.jetbrains.annotations.NotNull;
+import java.util.stream.Collectors;
 
 public class GameViewModel extends AndroidViewModel implements LifecycleObserver {
 
   private final GameRepository repository;
   private final MutableLiveData<Game> game;
-  private final MutableLiveData<String> pool;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
   private final SharedPreferences preferences;
   private final String basePool;
 
-  public GameViewModel(@NonNull @NotNull Application application) {
+  public GameViewModel(@NonNull Application application) {
     super(application);
     repository = new GameRepository(application);
     game = new MutableLiveData<>();
@@ -38,21 +37,16 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
     pending = new CompositeDisposable();
     preferences = PreferenceManager.getDefaultSharedPreferences(application);
     String[] emojis = application.getResources().getStringArray(R.array.emojis);
-    StringBuilder builder = new StringBuilder();
+    StringBuilder builder  = new StringBuilder();
     for (String emoji : emojis) {
       builder.append(emoji);
     }
     basePool = builder.toString();
-    pool = new MutableLiveData<>(basePool);
     startGame();
   }
 
   public LiveData<Game> getGame() {
     return game;
-  }
-
-  public LiveData<String> getPool() {
-    return pool;
   }
 
   public LiveData<Throwable> getThrowable() {
@@ -63,7 +57,7 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
     throwable.setValue(null);
     pending.add(
         repository
-            .create(pool.getValue(), getCodeLengthPref())
+            .create(getPoolPref(), getCodeLengthPref())
             .subscribe(
                 game::postValue,
                 this::handleThrowable
@@ -79,7 +73,8 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
             .addGuess(game.getValue(), text)
             .subscribe(
                 game::postValue,
-                this::handleThrowable)
+                this::handleThrowable
+            )
     );
   }
 
@@ -98,5 +93,16 @@ public class GameViewModel extends AndroidViewModel implements LifecycleObserver
     Resources res = context.getResources();
     return preferences.getInt(res.getString(R.string.code_length_pref_key),
         res.getInteger(R.integer.code_length_pref_default));
+  }
+
+  private String getPoolPref() {
+    Resources res = getApplication().getResources();
+    int poolSizePref = preferences.getInt(res.getString(R.string.pool_size_pref_key),
+        res.getInteger(R.integer.pool_size_pref_default));
+    return basePool
+        .codePoints()
+        .limit(poolSizePref)
+        .mapToObj((codePoint) -> new String(new int[]{codePoint}, 0, 1))
+        .collect(Collectors.joining());
   }
 }
